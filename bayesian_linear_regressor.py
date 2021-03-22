3cc722ae16e00d72d5266da3f181655ac79d14ad
Douglas Mason <douglasmason@gmail.com>
2021-03-22 11:22:51 -0700

    Update bayesian_linear_regressor.py

--- bayesian_linear_regressor.py
+++ bayesian_linear_regressor.py
@@ -2,9 +2,9 @@ import numpy as np
 
 class BayesLinearRegressor:
     
-    def __init__(self, n_features, alpha=1e6):
+    def __init__(self, number_of_features, alpha=1e6):
         '''
-        :param n_features: Integer number of features in the training rows, excluding the intercept and output values 
+        :param number_of_features: Integer number of features in the training rows, excluding the intercept and output values 
         :param alpha: Float inverse ridge regularizaiton constant, set to 1e6
         '''
         
@@ -14,11 +14,11 @@ class BayesLinearRegressor:
         #   Once you see that it's just updating the moment matrix and the sum of squared residuals, it's straightforward!
         #   So those are our internal variables that everything else depends upon
         
-        self.n_features = n_features
+        self.number_of_features = number_of_features
         self.alpha = alpha
-        self.beta_means = np.array([[0] * (n_features + 1)],dtype=np.float).T # + 1 for the intercept
+        self.beta_means = np.array([[0] * (number_of_features + 1)],dtype=np.float).T # + 1 for the intercept
         self.number_of_updates = 0
-        self.moment_matrix = np.eye(n_features + 2) * 0.0 # + 2 for the intercept and the output
+        self.moment_matrix = np.eye(number_of_features + 2) * 0.0 # + 2 for the intercept and the output
         self.residual_sum_squares = 0
         
     def partial_fit(self, X, Y, reverse=False):
@@ -35,7 +35,7 @@ class BayesLinearRegressor:
         
         moment_of_X_before = self.moment_matrix[:-1, :-1]
         beta_means_before = self.beta_means.copy()
-        inverted_covariance_matrix_before = moment_of_X_before + np.eye(self.n_features + 1) / self.alpha
+        inverted_covariance_matrix_before = moment_of_X_before + np.eye(self.number_of_features + 1) / self.alpha
         
         # Here we concatenate the intercept input value (constant 1), the input vector, and the output value:
         rank_1_update_vector = np.array([[1] + row + output for row, output in zip(X.tolist(), Y.tolist())])
@@ -51,7 +51,7 @@ class BayesLinearRegressor:
             
         moment_of_X = self.moment_matrix[:-1, :-1]
         moment_of_X_and_Y = self.moment_matrix[:-1, -1]
-        inverted_covariance_matrix = moment_of_X + np.eye(self.n_features + 1) / self.alpha
+        inverted_covariance_matrix = moment_of_X + np.eye(self.number_of_features + 1) / self.alpha
         covariance_matrix = np.linalg.inv(inverted_covariance_matrix)
         
         self.beta_means = covariance_matrix @ moment_of_X_and_Y
@@ -74,15 +74,24 @@ class BayesLinearRegressor:
         :param freeze_parameter_sample: Boolean. When set to True, we sample from the parameters only once for each prediction
         :return: 
         '''
+
+        scale_multiplier = 1.0 / max(1.0, (self.number_of_updates - self.number_of_features - 1))
+        
         if use_means:
             return self.beta_means.T @ X.T
         else:
             if freeze_parameter_sample:
                 if self.frozen_parameter_sample is not None:
-                    self.frozen_parameter_sample = np.random.multivariate_normal(self.beta_means.T[0], self.cov)
+                    self.frozen_parameter_sample =  np.random.multivariate_normal(
+                        self.beta_means.T[0], 
+                        self.residual_sum_squares * scale_multiplier * self.cov
+                    )
                 beta = self.frozen_parameter_sample
             else:
-                beta = np.random.multivariate_normal(self.beta_means.T[0], self.cov_params)
+                beta = np.random.multivariate_normal(
+                    self.beta_means.T[0], 
+                    self.residual_sum_squares * scale_multiplier * self.cov_params
+                )
             return beta.T @ X.T
         
     @property
@@ -99,6 +108,6 @@ class BayesLinearRegressor:
 
     @property
     def cov_params(self):
-        inverted_covariance_matrix = self.moment_of_X + np.eye(self.n_features + 1) / self.alpha
+        inverted_covariance_matrix = self.moment_of_X + np.eye(self.number_of_features + 1) / self.alpha
         return np.linalg.inv(inverted_covariance_matrix)
     
